3863ef0d84e62a12ac7e7f7ebd83048a4195e336
Steven Schlechte <steven.schlechte@gmx.net>
2024-10-25 09:46:49 +0200

    fixed linting errors

--- install_dev.sh
+++ install_dev.sh
@@ -4,11 +4,11 @@ echo "Install development environment."
 
 SOURCE=${BASH_SOURCE[0]}
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+       DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+       SOURCE=$(readlink "$SOURCE")
+       [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 
 set -e
 echo "Install python_template."
